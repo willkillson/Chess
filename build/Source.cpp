0491c95b59d5d8d469e19dbe3607da9d561ac556
willkillson <willkillson@gmail.com>
2017-11-15 20:33:10 -0700

    set up classes

--- build/Source.cpp
+++ build/Source.cpp
@@ -1,6 +1,30 @@
 
 #include <iostream>
 #include <Windows.h>
+#include "Board.h"
+#include "Piece.h"
+
+
+
+void MoveCursor(int x, int y) 
+{
+//This function moves the console cursor to the cordinates x,y where 0,0 is the top left
+	HANDLE hOut;
+	COORD Position;
+
+	hOut = GetStdHandle(STD_OUTPUT_HANDLE);
+
+	Position.X = x;
+	Position.Y = y;
+	SetConsoleCursorPosition(hOut, Position);
+
+
+	HANDLE consoleHandle = GetStdHandle(STD_OUTPUT_HANDLE);
+	CONSOLE_CURSOR_INFO info;
+	info.dwSize = 100;
+	info.bVisible = FALSE;
+	SetConsoleCursorInfo(consoleHandle, &info);
+}
 
 
 int main()
@@ -8,4 +32,6 @@ int main()
 
 	std::cout << "Hello world!" << std::endl;
 
+	MoveCursor(5, 5);
+	std::cout << "Hello world!" << std::endl;
 }
